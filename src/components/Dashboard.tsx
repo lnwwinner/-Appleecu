de04138a7d320b4a0272aa5d84386a8ab5c797d1
lnwwinner <113084556+lnwwinner@users.noreply.github.com>
2026-02-23 03:22:10 +0700

    Create ECU Tuning Dashboard component

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+    return (
+        <div>
+            <h1>ECU Tuning Dashboard</h1>
+            <p>Welcome to the ECU tuning dashboard!</p>
+            {/* Add chart components here */}
+            {/* Add tuning parameters here */}
+        </div>
+    );
+};
+
+export default Dashboard;
